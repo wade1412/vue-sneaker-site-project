7122ec9d796c6c5c7ccb39d15f2ded0e369321cd
Valeriy Petrenko <fourcher00@gmail.com>
2024-04-24 13:57:08 -0400

    Created ItemCard layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
+import ItemCard from './components/ItemCard.vue'
 import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <TheHeader />
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-10">
+    <TheHeader />
+
+    <div>
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+    </div>
+  </div>
 </template>
---
+++ src/components/ItemCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="relative m-10 border border-slate-100 rounded-xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
+  >
+    <img
+      src="D:\React Projects\vue-sneaker-site-project\public\img\liked.svg"
+      alt="Like 1"
+      class="absolute top-8 left-8"
+    />
+    <img
+      src="D:\React Projects\vue-sneaker-site-project\public\img\sneakers\1.jpg"
+      alt="Sneaker1"
+    />
+    <p class="mt-2">Nike Blazer Mid Suede Men</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Price:</span>
+        <span>$89</span>
+      </div>
+
+      <img src="D:\React Projects\vue-sneaker-site-project\public\img\plus.svg" alt="AddToCart" />
+    </div>
+  </div>
+</template>
