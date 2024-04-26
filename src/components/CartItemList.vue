6bf63e4a0a4b563ddd82e31a4ad916e679c9c63e
Valeriy Petrenko <fourcher00@gmail.com>
2024-04-26 10:54:57 -0400

    Cart Item List Layout created

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 flex-1 justify-between">
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </div>
+</template>
--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,14 +1,33 @@
 <script setup>
 import CartHead from './CartHead.vue'
-import CartItem from './CartItem.vue'
+import CartItemList from './CartItemList.vue'
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-50"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <CartHead />
-    <CartItem />
-    <CartItem />
-    <CartItem />
+
+    <CartItemList />
+
+    <div class="flex flex-col gap-4 my-7 bottom-0 content-evenly">
+      <div class="flex gap-2">
+        <span> Total:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>$111</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span> Shipping:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>$19</b>
+      </div>
+      <button
+        disabled=""
+        class="bg-blue-200 w-full rounded-xl py-3 text-xl font-bold text-white hover:bg-blue-300 transition active:bg-lime-500 disabled:bg-slate-300 cursor-pointer"
+      >
+        Confirm order
+      </button>
+    </div>
   </div>
 </template>
