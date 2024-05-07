332fd6f93b01375f9ecfe3aa350eb33185c7253a
Valeriy Petrenko <fourcher00@gmail.com>
2024-05-07 15:24:43 -0400

    Made Info Block to display when the cart is empty

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CartHead from './CartHead.vue'
 import CartItemList from './CartItemList.vue'
+import EmptyCartInfo from './EmptyCartInfo.vue'
 
 const emit = defineEmits(['createOrder'])
 
@@ -16,27 +17,37 @@ defineProps({
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <CartHead />
 
-    <CartItemList />
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <EmptyCartInfo
+        title="Your cart is empty"
+        description="Go pick yourself a nice pair to make an order!"
+        imageUrl="/public/img/empty-cart.jpg"
+      />
+    </div>
 
-    <div class="flex flex-col gap-4 my-7 bottom-0 content-evenly">
-      <div class="flex gap-2">
-        <span> Total:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>${{ totalPrice }}</b>
-      </div>
+    <div v-else>
+      <CartItemList />
 
-      <div class="flex gap-2">
-        <span> Tax:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>${{ vatPrice }}</b>
+      <div class="flex flex-col gap-4 my-7 bottom-0 content-evenly">
+        <div class="flex gap-2">
+          <span> Total:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>${{ totalPrice }}</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span> Tax:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>${{ vatPrice }}</b>
+        </div>
+        <button
+          :disabled="isCartButtonDisabled"
+          class="bg-blue-300 w-full rounded-xl py-3 text-xl font-bold text-white hover:bg-blue-400 transition active:bg-blue-500 disabled:bg-slate-300 cursor-pointer"
+          @click="() => emit('createOrder')"
+        >
+          Confirm order
+        </button>
       </div>
-      <button
-        :disabled="isCartButtonDisabled"
-        class="bg-blue-300 w-full rounded-xl py-3 text-xl font-bold text-white hover:bg-blue-400 transition active:bg-blue-500 disabled:bg-slate-300 cursor-pointer"
-        @click="() => emit('createOrder')"
-      >
-        Confirm order
-      </button>
     </div>
   </div>
 </template>
